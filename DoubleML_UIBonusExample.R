7513bc96c15e1f15dd1d1c90338a8b18ab0e8eaf
Jérémy <Jeremy.L.Hour@ensae.fr>
2018-01-11 17:35:01 +0100

    DML Empirical Example
    with Lasso and Random Forest

---
+++ DoubleML_UIBonusExample.R
@@ -0,0 +1,105 @@
+### Double ML: UI Bonus Example
+### Jeremy LHour
+### 08/01/2018
+
+### This is an empirical example from Chernozhukov et al. (2017) procedure to
+### Estimate ATT assuming heteregeneous effects.
+### Data is from Bilias (2000) on the effect of Unemployment Insurance Bonus on
+### Unemployment duration.
+
+### See Chernozhukov's paper for more complete results.
+
+
+### Set working directory
+setwd("//ulysse/users/JL.HOUR/1A_These/B. ENSAE Classes/Cours3A/hdmetrics")
+
+rm(list=ls())
+set.seed(12071990)
+
+
+### 0. Settings
+
+### Packages
+library("hdm")
+library("randomForest")
+
+### Load user-defined functions
+source("functions/ATE.R")
+
+### Pennsylvania Bonus Data
+Penn = as.data.frame(read.table("packages/DMLonGitHub-master/penn_jae.dat", header=T ))
+
+index = (Penn$tg==0) | (Penn$tg==4) # Only keep treatment=4 and controls
+data = Penn[index,]
+data$tg[data$tg==4] = 1
+data[,"dep1"] = data$dep==1
+data[,"dep2"] = data$dep==2
+data$inuidur1 = log(data$inuidur1)
+
+
+Y = data[,"inuidur1"] # Outcome
+D = data[,"tg"] # Treatment
+X = as.matrix(data[,c("female","black","othrace","dep1","dep2","q2","q3","q4","q5","q6","agelt35","agegt54","durable","lusd","husd")]) # Covariates
+
+K = 5
+n = length(Y)
+
+split = runif(n)
+cvgroup = as.numeric(cut(split,quantile(split,probs = seq(0, 1, 1/K)),include.lowest = T))
+
+
+### 1. Simple DML w Lasso and Random Forest
+thetaL = vector(length=K); psiL = vector(length=n)
+thetaRF = vector(length=K); psiRF = vector(length=n)
+
+for(k in 1:K){
+  Ik = cvgroup==k # Split the sample
+  NIk = cvgroup!=k
+  
+  # A. Propensity Score
+  treatfit = rlassologit(D[NIk] ~ X[NIk,]) # Logit Lasso
+  mhatL = predict(treatfit,newdata=X[Ik,])
+  
+  optRF = tuneRF(X[NIk,],as.factor(D[NIk]),stepFactor=1.5, improve=0.05, nodesize=5, doBest=T, plot=F, trace=F)
+  min = optRF$mtry
+  treatForest = randomForest(X[NIk,],as.factor(D[NIk]), mtry=min)
+  mhatRF = predict(treatForest, X[Ik,], type="prob")[,2]
+  
+  # B. E(Y_0 \vert X)
+  outcomefit0 = rlasso(Y[D[NIk]==0] ~ X[D[NIk]==0,]) # Lasso
+  g0hatL = predict(outcomefit0,newdata = X[Ik,])
+  
+  optRF = tuneRF(X[D[NIk]==0,],Y[D[NIk]==0],stepFactor=1.5, improve=0.05, nodesize=5, doBest=T, plot=F, trace=F)
+  min = optRF$mtry
+  outcomeForest0 = randomForest(X[D[NIk]==0,],Y[D[NIk]==0], mtry=min)
+  g0hatRF = predict(outcomeForest0, X[D[Ik],], type="response")
+  
+  # C. ATT
+  thetaL[k] = ATT(Y[Ik],D[Ik],g0hatL,mhatL)
+  psiL[Ik] = SE.ATT(Y[Ik],D[Ik],g0hatL,mhatL)$psi
+  
+  thetaRF[k] = ATT(Y[Ik],D[Ik],g0hatRF,mhatRF)
+  psiRF[Ik] = SE.ATT(Y[Ik],D[Ik],g0hatRF,mhatRF)$psi
+}
+
+### 3. Results
+
+## A. Lasso
+SE.thetaL = sd(psiL)/(mean(D)*sqrt(n))
+
+print("ATT, Point estimate")
+print(mean(thetaL))
+print("Standard Error")
+print(SE.thetaL)
+print(".95 Confidence Interval")
+print(paste("[",mean(thetaL)+qnorm(.025)*SE.thetaL,";",mean(thetaL)+qnorm(.975)*SE.thetaL,"]"))
+
+### B. Random Forest
+SE.thetaRF = sd(psiRF)/(mean(D)*sqrt(n))
+
+print("ATT, Point estimate")
+print(mean(thetaRF))
+print("Standard Error")
+print(SE.thetaRF)
+print(".95 Confidence Interval")
+print(paste("[",mean(thetaRF)+qnorm(.025)*SE.thetaRF,";",mean(thetaRF)+qnorm(.975)*SE.thetaRF,"]"))
--- functions/ATE.R
+++ functions/ATE.R
@@ -1,13 +1,15 @@
-#' ATE and ATT
+#' ATE and ATT functions
 #'
-#' Computes ATE and ATT from data and nuisance parameters as in DoubleML p.35
-#' Last edited: 05/01/2018
+#' Computes ATE and ATT from data and nuisance parameters as in DoubleML p.35 (Chernozhukov et al., 2017)
+#' Last edited: 11/01/2018
 #' 
 #' @param y vector of the dependent variable
 #' @param D vector of treatment assignment
 #' @param g1 estimate of E(Y_1 \vert X)
 #' @param g0 estimate of E(Y_0 \vert X)
 #' @param m propensity score estimate P(D=1 \vert X)
+#' 
+#' All vectors above must have the same length.
 #' 
 #' @author Jeremy LHour
 
@@ -32,5 +34,6 @@ SE.ATT <- function(Y,D,g0,m){
   n = length(Y)
   pi = mean(D)
   psi = D*(Y-g0)/pi - (1-D)*m*(Y-g0)/(pi*(1-m))
-  return(sd(psi)/(pi*sqrt(n)))
+  return(list(SE = sd(psi)/(pi*sqrt(n)),
+              psi = psi))
 }
